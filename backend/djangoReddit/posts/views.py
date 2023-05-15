9b5cc5c0ad72f32a352aee75c3572679485b74b5
IDerdi <erdincyaralli@gmail.com>
2023-05-15 12:32:45 +0300

    inşallah cıkmaz

--- backend/djangoReddit/posts/views.py
+++ backend/djangoReddit/posts/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts=Post.objects.all()
+    context={
+        'posts':posts
+    }
+    return render(request, 'index.html',context)
 
 def begeni(request):
     return render(request, 'begenilerim.html')

2485de3657b46978cd5a0532a3bd8115847088ac
IDerdi <erdincyaralli@gmail.com>
2023-05-21 10:55:56 +0300

    pazar

--- backend/djangoReddit/posts/models.py
+++ backend/djangoReddit/posts/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100 , null=True)
     content = models.TextField(max_length=1000 )
     image=models.FileField( upload_to='posts/' , null=True, blank=True )
+    created_at = models.DateTimeField(auto_now_add=True,null=True, verbose_name='oluşturulma tarihi')
 
     def __str__(self):
         return self.title
